a198601b04e2a830efeadaa14364b0c0b748e5ec
Xavi <xavi@xaviesteve.com>
2016-06-30 19:02:55 +0200

    'User' table

---
+++ _install/04-create-table-user.sql
@@ -0,0 +1,19 @@
+CREATE TABLE `user` (
+	`id` int(11) NOT NULL,
+	`username` varchar(30) NOT NULL,
+	`full_name` varchar(100) NOT NULL,
+	`email` varchar(50) NOT NULL,
+	`avatar` varchar(250) NOT NULL,
+	`session_token` varchar(250) NOT NULL,
+	`request_token` varchar(250) NOT NULL,
+	`request_token_secret` varchar(250) NOT NULL,
+	`date_added` datetime NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+ALTER TABLE `user`
+	ADD PRIMARY KEY (`id`),
+	ADD UNIQUE KEY `username` (`username`),
+	ADD UNIQUE KEY `email` (`email`);
+
+ALTER TABLE `user`
+	MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=1;
