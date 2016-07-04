ebe8868e14ffec87e8519eb3a3bb5d2d0ecd6bdd
Xavi <xavi@xaviesteve.com>
2016-07-04 23:05:21 +0200

    date_default_timezone_set

--- application/config/config.php
+++ application/config/config.php
@@ -46,6 +46,10 @@ define('URL_DOMAIN', $_SERVER['HTTP_HOST'] );
 define('URL_SUB_FOLDER', str_replace(URL_PUBLIC_FOLDER, '', dirname($_SERVER['SCRIPT_NAME'])) );
 define('URL', URL_PROTOCOL . URL_DOMAIN . URL_SUB_FOLDER );
 
+
+date_default_timezone_set('Europe/Madrid');
+
+
 /**
 * Configuration for: Database
 * This is the place where you define your database credentials, database type etc.
